270e45bfc06552d2e4341941820b7ed38cd543a4
Mike <nottamike92@gmail.com>
2019-01-25 16:10:29 +0100

    save.rb done

---
+++ lib/application.rb
@@ -0,0 +1,16 @@
+require 'save'
+require 'scrapper'
+require 'gmail'
+require 'view'
+
+class Application
+
+	def initialize
+	view = View.new
+	view.menu 
+	# 1 Méthode View qui afficher un menu : - choisis ton département
+#										- choisis le nombres de villes pour leur envoyer des emails
+
+    end
+
+end
