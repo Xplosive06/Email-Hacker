eba9874baa16928be16fe84fc43fb4898ec92610
Mike <nottamike92@gmail.com>
2019-01-25 14:12:48 +0100

    scrapper + gemfile
    Add methods from scrapping app
    
    Add Nokogiri gem

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source 'https://rubygems.org'
-ruby '2.5.1'
 
 gem 'pry'
 gem 'rubocop'
 gem 'rspec'
 gem 'dotenv'
-#gem 'csv'
+gem 'nokogiri'
 gem 'gmail'
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,15 +1,36 @@
-class Scrapper
-	# scrapper 3 départements
 
-	def return_city_array
-		
-	end
+  # scrapper 3 départements
 
-	def return_email_array
-		
-	end
+  def get_cities_names(state_name, state_number)
+    array_of_cities = Array.new
+    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/" + state_name))
+    page.css('a[href*="./state_number/"]').each do |node|
+      array_of_cities << node.text.downcase.tr(" ", "-")
 
-	def return_state_array
-		
-	end
-end
+    end
+    return array_of_cities
+  end
+
+  def get_cities_emails(cities_names, state_number)
+    cities_array = Array.new
+    i = 0
+    for city in cities_names
+      i += 1
+      puts i
+      page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/" + state_number +"/" + city))
+      #"https://www.annuaire-des-mairies.com/95/avernes.html"
+      city_email = page.css('tbody tr:nth-child(4) td:nth-child(2)')
+      
+      city_email = city_email[0]
+
+      cities_array << city_email.text
+
+    end
+    return cities_array
+  end
+
+  def return_state_array
+
+  end
+
+puts get_cities_names("bas-rhin", 67)
